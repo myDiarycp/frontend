9de3b9155b699f4e0936d61e03b3ea8eae953626
jeremiahpino <jpino1999@gmail.com>
2022-11-23 17:25:27 -0800

    resolved some merge conflicts

--- src/routes/DayEvent.js
+++ src/routes/DayEvent.js
@@ -6,37 +6,22 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import { Button } from 'react-bootstrap';
 // import Navbar from 'react-bootstrap/Navbar';
-<<<<<<< HEAD
 import Sidebar from './/Sidebar';
 import Entry from './/Entry';
 import Home from '../home';
 import Rating from './/Rating';
 
-=======
-import Sidebar from './/Sidebar'
 import React, { useState, useEffect } from 'react'
 import Quill from 'quill'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import Alert from 'react-bootstrap/Alert';
 import axios from "axios";
->>>>>>> main
 
 export default function Journal() {
     const domain=process.env.REACT_APP_API_DOMAIN //"http://localhost:8080"
     const location = useLocation();
     let date_object = location.state;
-<<<<<<< HEAD
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    // console.log(days[now.getDay()] + ' ' + months[now.getMonth()] + ' ' + now.getDate() + ' ' + now.getFullYear()); //Tuesday February 12 2013
-    // let date = (date_object.getMonth() + 1) + "/" + date_object.getDate() + "/" + date_object.getFullYear();
-    let date1 = days[date_object.getDay()] + ' ' + months[date_object.getMonth()] + ' ' + date_object.getDate() + ', ' + date_object.getFullYear();
-
-    // access user object
-    let user=JSON.parse(localStorage.getItem("user"))
-   
-=======
     let date = (date_object.getMonth() + 1) + "-" + date_object.getDate() + "-" + date_object.getFullYear();
     const Diary=JSON.parse(localStorage.getItem("userData")).diary
     const DiaryMap = new Map(Object.entries(Diary));
@@ -77,7 +62,6 @@ export default function Journal() {
         window.location.href = '/calendar';
 
     }
->>>>>>> main
     return (
         // <h1>{date}</h1>
         <div className = "DayEvent">
@@ -88,32 +72,25 @@ export default function Journal() {
                     </h1>
                     <Sidebar />
                 </Col>
-<<<<<<< HEAD
                 <Col md={4} className="middle">
-                    <h1 className="mtitle">Welcome to Your Diary {user.name} </h1>
+                    <h1 className="mtitle">Welcome to Your Diary userData.name </h1>
                 </Col>
                 <Col md={6} className = "entrySide">
-                    <h1 className="aboveEntry">Entry Date: {date1}</h1>
+                    <h1 className="aboveEntry">Entry Date: {date}</h1>
                     <h1 className="ratingTitle"> <u>Day Rating:</u></h1>
                     <Rating className="rating"/>
-                    <Entry className="textEditor" />
-=======
-                <Col className = "right">{date}
-                <ReactQuill theme="snow" value={value} onChange={setValue} />
-                
-
-
-      <Button onClick={submitEntry}
-      variant="dark"
-      size="lg"
+                    <ReactQuill theme="snow" value={value} onChange={setValue} />
+                    <Button onClick={submitEntry}
+                    variant="dark"
+                    size="lg"
       
-      >Submit</Button>
-      
->>>>>>> main
+                    >Submit</Button>
                 </Col>
             </Row>
         </div>
 
     );
+
+
     
 }
