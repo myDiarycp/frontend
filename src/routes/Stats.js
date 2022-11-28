d4b5addeedf9450c7d07bf1ea3f763ddf4dbba75
connor <connor.h.dye@gmail.com>
2022-11-28 13:49:24 -0800

    =Stats page updated and working

--- src/routes/Stats.js
+++ src/routes/Stats.js
@@ -6,18 +6,48 @@ import {CanvasJSChart} from 'canvasjs-react-charts'
 // var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 export default function Stats() {
+    let currentDate = new Date();
+    let currentMonth = currentDate.getMonth() + 1
+    console.log(currentMonth)
+    const Diary=JSON.parse(localStorage.getItem("userData")).diary
+    const DiaryMap = new Map(Object.entries(Diary));
+    let y1 = 0;
+    let y2 = 0;
+    let y3 = 0;
+    let y4 = 0;
+    let y5 = 0;
+
+    for (const [key, value] of DiaryMap.entries()) {
+        console.log(key, value.rating);
+        const myArray = key.split("-");
+        console.log(myArray[0])
+        if (parseInt(myArray[0]) === currentMonth) {
+            console.log("entered")
+            if ( parseInt(value.rating) === 1)
+                y1 = y1 + 1
+            if ( parseInt(value.rating) === 2)
+                y2 = y2 + 1
+            if ( parseInt(value.rating) === 3)
+                y3 = y3 + 1    
+            if ( parseInt(value.rating) === 4)
+                y4 = y4 + 1   
+            if ( parseInt(value.rating) === 5)
+                y5 = y5 + 1 
+        }
+    }
+    console.log("Y1 IS", y1)
     const options = {
         title: {
-          text: "Statistics"
+          text: "Statistics for month " + currentMonth
         },
         data: [{				
                   type: "column",
                   dataPoints: [
-                      { label: "1 Star",  y: 5  },
-                      { label: "2 Star", y: 5  },
-                      { label: "3 Star", y: 5  },
-                      { label: "4 Star",  y: 5  },
-                      { label: "5 Star",  y: 5  }
+                      { label: "1 Star",  y: y1  },
+                      { label: "2 Star", y: y2  },
+                      { label: "3 Star", y: y3  },
+                      { label: "4 Star",  y: y4  },
+                      { label: "5 Star",  y: y5  }
                   ]
          }]
      }
