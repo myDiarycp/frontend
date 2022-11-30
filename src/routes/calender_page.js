18b0d95c9e75146e5f22ccf9074e14ceeec41f86
ynozar <ynozar@calpoly.edu>
2022-11-29 21:08:36 -0800

    yo

--- src/routes/calender_page.js
+++ src/routes/calender_page.js
@@ -18,6 +18,7 @@ export default function CalendarDisplay() {
     const Diary=JSON.parse(localStorage.getItem("userData")).diary
     const DiaryMap = new Map(Object.entries(Diary));
     const [date, setDate] = useState(new Date());
+    const [count, setCount] = useState(Math.random());
     let navigate = useNavigate();
     const routeChange = (curr_date) => {
         let path = '/calendar/day';
@@ -25,8 +26,33 @@ export default function CalendarDisplay() {
         navigate(path, { state: curr_date });
     };
     var grad=['0','#ff222c',"#ff7b80",'#96ed89','#45BF55','#168039']
+
+    function loadColors(){
+        console.log("e")
+        const dateButtons = document.querySelectorAll(".react-calendar__tile");
+        dateButtons.forEach((element)=>{
+            //console.log(element.children[0].getAttribute("aria-label"))
+            var dt=new Date(element.children[0].getAttribute("aria-label")).toLocaleDateString("en-US").replace(/\//g, '-');
+            //console.log(dt)
+            var entry=DiaryMap.get(dt)
+            console.log(entry)
+            
+            if(entry&&dt!=new Date().toLocaleDateString("en-US").replace(/\//g, '-')){
+                console.log(grad[(entry.rating)])
+                element.style.background=grad[entry.rating]}
+
+            }
+            
+            
+            
+            )
+
+    }
+
+
     useEffect(()=>
     {
+        console.log(date)
         const dateButtons = document.querySelectorAll(".react-calendar__tile");
         dateButtons.forEach((element)=>{
             //console.log(element.children[0].getAttribute("aria-label"))
@@ -45,7 +71,7 @@ export default function CalendarDisplay() {
             
             )
         //[0].style.background='#FFFFFF';
-    }) 
+    })   
 
     return (
         <div>
@@ -60,7 +86,7 @@ export default function CalendarDisplay() {
                     <Calendar
                     onChange={setDate}
                     value={date}
-                    onClickDay={(value) => routeChange(value)}
+                    onClickDay={(value) => {routeChange(value);}}
                     />
                 </Col>
             </Row>
